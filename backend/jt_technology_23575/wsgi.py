673a3d5c19ce6848991b63ac8f50e386f53ba49a
Crowdbotics <team@crowdbotics.com>
2020-12-28 13:12:01 +0000

    Initial commit.

---
+++ backend/jt_technology_23575/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jt_technology_23575 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jt_technology_23575.settings')
+
+application = get_wsgi_application()
